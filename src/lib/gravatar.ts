134ce3654c186b1a93cf3fec7b3192ddbdf52122
JarrodMalkovic <jarrodmalkovic@outlook.com>
2024-04-19 21:10:48 +1000

    Remove unncessary comment

--- src/lib/gravatar.ts
+++ src/lib/gravatar.ts
@@ -4,5 +4,5 @@ export function getGravatarUrl(email: string) {
 	const trimmedEmail = email.trim().toLowerCase();
 	const hash = md5(trimmedEmail);
 
-	return `https://www.gravatar.com/avatar/${hash}?d=identicon&s=200`; // 'identicon' is the default, 's' is the size parameter
+	return `https://www.gravatar.com/avatar/${hash}?d=identicon&s=200`;
 }
